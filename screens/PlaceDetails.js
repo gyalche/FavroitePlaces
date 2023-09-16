6702788d1ef2b25800444d8d9a8948842f56df01
dawa sherpa <sherpadawag1998@gmail.com>
2023-09-16 19:58:31 +0545

    place details complete

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import React from 'react';
+import OutlineButton from '../UI/OutlineButton';
+import { Colors } from '../constants/colors';
 
-export default function PlaceDetails() {
+export default function PlaceDetails({ route }) {
+  function showOnMapHandler() {}
+  const selectedPlaceId = route.params.placeId;
+  useEffect(() => {}, [selectedPlaceId]);
   return (
-    <View>
-      <Text>PlaceDetails</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>ADDRESS</Text>
+        </View>
+        <OutlineButton icon="map" onPress={showOnMapHandler}>
+          View on Map
+        </OutlineButton>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: '35%',
+    minHeight: 300,
+    width: '100%',
+  },
+  locationContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: Colors.primary500,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
